18079f2c6fd08b3901ffffda4c9780a78f6d18b7
Steven <stevenalfred93@yahoo.com>
2018-05-16 23:33:55 -0400

    Added the currency symbol to text fields

--- WindowShopper/Views/CurrencyTxtField.swift
+++ WindowShopper/Views/CurrencyTxtField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable //class attribute that enables code in class to show up in interface builder
 class CurrencyTxtField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2) - size/2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8480568131, green: 0.8480568131, blue: 0.8480568131, alpha: 0.7964469178)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = UIColor.darkGray
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() { //Function that makes InterfaceBuilder appear as it is suppose to at runtime.
         customizeView()
     }
@@ -24,6 +40,8 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25) //used Color Literal to control opacity as well and just to experiment
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         //if placeholder is nill it wont go into if
         if let p = placeholder{
